5eb44c579e417b7cd19df0f748adc3eb0fd3bbc9
Faraz <farazpourebrahimh@gmail.com>
2025-05-23 14:30:24 +0330

    feat(Routes): Add Routes Files

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
