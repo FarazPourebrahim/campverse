0f93f1bc432b29a8d411867887782d5e3cad0a2c
Faraz <farazpourebrahimh@gmail.com>
2025-05-28 09:09:49 +0330

    feat(App): Add Middlewares

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+//@desc     Logs request to console
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}:/${req.url} ${res.statusCode}`);
+    next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -7,6 +8,11 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger);
+}
+
 
 app.use('/api/v1/bootcamps', bootcamps);
 
