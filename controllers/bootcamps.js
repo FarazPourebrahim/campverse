bee66e731d97d1a5c3f2becf7cff3caca3f1bc22
faraz <farazpourebrahimh@gmail.com>
2025-08-08 16:18:57 +0330

    feat(Bootcamps): Add Pagination, Filters, Select Fields and Sort to Bootcamps API

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,11 +6,77 @@ const asyncHandler = require("../middleware/async");
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  // Filtering
+  let queryObj = { ...req.query };
+  const removeFields = ["select", "sort", "page", "limit"];
+  removeFields.forEach((param) => delete queryObj[param]);
+
+  let query = Bootcamp.find();
+
+  for (let key in queryObj) {
+    const match = key.match(/^(\w+)\[(\w+)\]$/);
+    if (match) {
+      const field = match[1];
+      const operator = match[2];
+      if (["gt", "gte", "lt", "lte", "in"].includes(operator)) {
+        const value =
+          operator === "in" ? queryObj[key].split(",") : Number(queryObj[key]);
+        if (
+          (value !== null && value !== undefined && !isNaN(value)) ||
+          operator === "in"
+        ) {
+          query = query.where(field)[operator](value);
+        }
+      }
+    } else {
+      query = query.where(key).equals(queryObj[key]);
+    }
+  }
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+  query = query.skip(startIndex).limit(limit);
+
+  const bootcamps = await query;
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
