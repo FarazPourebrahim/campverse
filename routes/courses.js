d52158d06bf8180f907cd3f27f2b70de82339272
faraz <farazpourebrahimh@gmail.com>
2025-08-22 18:26:06 +0330

    feat(Courses): Add Single Course API

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getCourses } = require("../controllers/Courses");
+const { getCourses, getCourse } = require("../controllers/Courses");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses);
+router.route("/:id").get(getCourse);
 
 module.exports = router;
